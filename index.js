94f96004a27b5363f3eec18e0e132af3e9d8275b
Kamran550 <kamranferzeliyev01@gmail.com>
2025-05-14 17:56:40 +0400

    ddewdwe

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
-app.get('/api', (req, res) => res.send('Hello from backend!'));
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.get("/api", (req, res) => {
+  const count = req.query.count;
+  console.log("req geldi");
+
+  res.json({ message: "Data received from React!", count });
+});
+app.listen(5000, () => console.log("Server running on port 5000"));
