5ce4e734ba9ba0f49497d2173a26b806d9ad360b
Kamran550 <kamranferzeliyev01@gmail.com>
2025-05-14 16:25:02 +0400

    push node

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.get('/api', (req, res) => res.send('Hello from backend!'));
+app.listen(5000, () => console.log('Server running on port 5000'));
